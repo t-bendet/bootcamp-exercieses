e602b9faf440604b2535fdcd73550c4067c37c32
t-bendet <talbendet21@gmail.com>
2021-02-16 09:19:34 +0200

    work

--- java-script/tester.js
+++ java-script/tester.js
@@ -15,34 +15,14 @@ const school = {
       capacityLeft: 2,
     },
   ],
-  students: [
-    { id: 10, name: "Jennifer", age: 20 },
-    { id: 11, name: "Howard", age: 23 },
-    { id: 12, name: "Old-Timmy", age: 86 },
-    { id: 13, name: "Houston", age: 21 },
-  ],
   findPerson(type,id){
     return this[type].find(obj => obj.id == id);
   },
-  assignStudent(id, subject) {
-    const student = this.findPerson('students', id);
-    const teacher = this.teachers.find((teacher) =>
-        teacher.subjects.includes(subject)
-    );
-    if (teacher && teacher.capacityLeft) {
-        teacher.students.push(student);
-        teacher.capacityLeft -= 1;
-    } else {
-        console.log('Sorry, no available teachers left');
-    }
-},
 
 };
 
-
-school.assignStudent(10, "history")
-
-// for (const [key, value] of Object.entries(this.teachers[0])) {
-//   console.log(`${key}: ${value}`);
-// }
+const x = school.findPerson("teachers",1)
+console.log(x)
+delete x.capacityLeft
+console.log(x)
 // if(this.teachers[0].subjects.includes(subject) && this.teachers[0].capacityLeft > 0)
