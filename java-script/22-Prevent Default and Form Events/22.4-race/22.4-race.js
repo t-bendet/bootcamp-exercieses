6f917946084135e19a4d07aa45a6d81e60bf5b90
t-bendet <talbendet21@gmail.com>
2021-02-24 16:40:00 +0200

    more

--- java-script/22-Prevent Default and Form Events/22.4-race/22.4-race.js
+++ java-script/22-Prevent Default and Form Events/22.4-race/22.4-race.js
@@ -1,18 +1,23 @@
-//We need a way for JavaScript to update the board state.
-//Createsimple JavaScript functions that update’s a player’s position.
-//To move the car’s do not use the click event. Use the ‘keyup’ event.
 //Find a way to announce the winner of the race.●Find a way to restart the game.
-const game = document.querySelector("html");
 const course1 = document.querySelector("#player1-race");
 const course2 = document.querySelector("#player2-race");
 const keyFunk = (e, key) => {
-  if (e.code === "ArrowRight"){
-    course1.children.
-  };
-  if (e.code === "Space") console.log("Space ");
+  if (e.code === "ArrowRight") {
+    let currPlace = course1.getElementsByClassName("active")[0];
+    let nextPlace = currPlace.nextElementSibling;
+    currPlace.classList.remove("active");
+    nextPlace.classList.add("active");
+    if (nextPlace.classList.contains("finish")) {
+      document.removeEventListener("keydown", keyFunk);
+      const again = confirm(
+        `thw winner is fdfd, would you like to play another game?`
+      );
+      if (again) {
+        location.reload();
+      }
+    }
+  }
+  // if (e.code === "Space") console.log("Space ");
 };
-game.addEventListener("keydown", keyFunk);
+document.addEventListener("keydown", keyFunk);
 // TODO dry code make the function for two rows
-
-course1.children[1].classList.add("active"); //move for
-course1.children[0].classList.remove("active"); //delete last td
