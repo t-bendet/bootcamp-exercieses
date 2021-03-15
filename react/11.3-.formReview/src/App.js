284ab6bc2bfb572da6407ac38c3453af87b79709
t-bendet <talbendet21@gmail.com>
2021-03-15 18:51:40 +0200

    intializing

---
+++ react/11.3-.formReview/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Counter from "./Counter";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <Counter />
+      </div>
+    );
+  }
+}
+export default App;
