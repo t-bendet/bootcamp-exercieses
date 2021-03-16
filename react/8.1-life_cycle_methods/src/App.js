21f7d70ec7358b993769b98a70bf16b4659e514f
t-bendet <talbendet21@gmail.com>
2021-03-16 10:31:41 +0200

    8.1

--- react/8.1-life_cycle_methods/src/App.js
+++ react/8.1-life_cycle_methods/src/App.js
@@ -1,14 +1,28 @@
 import React from "react";
 import "./App.css";
-import Counter from "./Counter";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { favoriteColor: "red" };
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ favoriteColor: "blue" });
+    }, 1000);
+  }
+  componentDidUpdate() {
+    const tal = document.querySelector("#tal");
+    tal.innerText = `The updated favorite color is ${this.state.favoriteColor}`;
+  }
   render() {
     return (
       <div className="container">
-        <Counter />
+        <h1>{`my favorite color is ${this.state.favoriteColor}`}</h1>
+        <div id="tal"></div>
       </div>
     );
   }
 }
+
 export default App;
