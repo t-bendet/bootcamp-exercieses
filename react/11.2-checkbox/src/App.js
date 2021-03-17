b31be1c46c7d58f1827644dfd08489b079b9af1d
t-bendet <talbendet21@gmail.com>
2021-03-17 13:15:29 +0200

    11.2

--- react/11.2-checkbox/src/App.js
+++ react/11.2-checkbox/src/App.js
@@ -5,6 +5,14 @@ class App extends React.Component {
   render() {
     return (
       <div className="container">
+        <form>
+          <input type="checkBox" checked={true}></input>i read the terms of the
+          app
+          <input type="checkBox" checked={true}></input>i accept the term of the
+          app
+          <input type="checkBox"></input>i want to get the weekly news letter
+          <input type="checkBox"></input>i want to get sales and offers
+        </form>
       </div>
     );
   }
