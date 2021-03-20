e581f8ead91b313e3b1efeb5bd828f720c19b1e8
t-bendet <talbendet21@gmail.com>
2021-03-20 22:00:41 +0200

    updating

---
+++ react/12.5/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+      </div>
+    );
+  }
+}
+export default App;
--- react/reactapi/src/components/App.js
+++ react/reactapi/src/components/App.js
@@ -1,21 +1,26 @@
 import React from "react";
-import axios from "axios";
+import Unsplash from "../Api/Unsplash";
 import SearchBar from "./SearchBar";
+import ImageList from "./ImageList";
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    axios.get("https://api.unsplash.com/search/photos", {
+  state = { images: [] };
+
+  onSearchSubmit = async (term) => {
+    const response = await Unsplash.get("/search/photos", {
       params: { query: term },
-      headers: {
-        Authorization: "Client-ID SH7dYrT5CyYwkcdXEvLZWXtZzDgfcqz0hmwPYpvDlGc",
-      },
     });
-  }
+    this.setState({ images: response.data.results });
+  };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
-        <SearchBar onSubmit={this.onSearchSubmit} />
+        <SearchBar
+          onSubmit={this.onSearchSubmit}
+          guesswhtIam="Im the propsobject"
+        />
+        <ImageList images={this.state.images} />
       </div>
     );
   }
