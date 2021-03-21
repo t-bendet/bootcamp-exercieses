b243ad18e91d1e7409f7e7443d858c6b0837b625
t-bendet <talbendet21@gmail.com>
2021-03-21 16:24:49 +0200

    updating

--- react/13.2-avatar/src/App.js
+++ react/13.2-avatar/src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
+import axios from "axios";
 import "./App.css";
 
+
+
 class App extends React.Component {
+  state = { avatars: [] };
+  getAvatars = async () => {
+    const test = await axios.get("https://randomuser.me/api/?results=10");
+    this.setState({ avatars: test.data.results });
+  };
+  componentDidMount() {
+    this.getAvatars();
+  }
   render() {
     return (
       <div className="container">
