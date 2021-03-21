6706e4d7ef4adcc433b19c4bbb810a0254225a18
t-bendet <talbendet21@gmail.com>
2021-03-21 18:41:29 +0200

    14.1

--- react/14.1-focus/src/App.js
+++ react/14.1-focus/src/App.js
@@ -2,9 +2,19 @@ import React from "react";
 import "./App.css";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.textInput = React.createRef();
+  }
+
+  componentDidMount() {
+    this.textInput.current.focus();
+  }
+
   render() {
     return (
-      <div className="container">
+      <div>
+        <input type="text" ref={this.textInput} />
       </div>
     );
   }
