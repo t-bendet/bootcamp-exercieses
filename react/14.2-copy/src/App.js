13dd570de80bda7699fb14361bf58fd2d0615124
t-bendet <talbendet21@gmail.com>
2021-03-21 19:07:10 +0200

    14.2

--- react/14.2-copy/src/App.js
+++ react/14.2-copy/src/App.js
@@ -2,9 +2,25 @@ import React from "react";
 import "./App.css";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.textInput = React.createRef();
+  }
+  copyMe = (e) => {
+    e.preventDefault();
+    let copyText = this.textInput.current;
+    copyText.select();
+    document.execCommand("copy");
+    alert("Copied the text: " + copyText.value);
+  };
   render() {
     return (
       <div className="container">
+        <form onSubmit={this.copyMe}>
+          <label>what is the meaning of life</label>
+          <textarea ref={this.textInput}></textarea>
+          <button type="submit">copy</button>
+        </form>
       </div>
     );
   }
