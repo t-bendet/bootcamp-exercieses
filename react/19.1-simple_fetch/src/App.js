64acd2b3acbc7c5cd7f3dda29cad79722b3b1d9a
t-bendet <talbendet21@gmail.com>
2021-04-01 21:46:43 +0300

    19.1

--- react/19.1-simple_fetch/src/App.js
+++ react/19.1-simple_fetch/src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-const App = ()=> {
+const App = () => {
+  const [movieInfo, setMovieInfo] = useState(undefined);
+  useEffect(() => {
+    const startWars = async () => {
+      const info = await axios.get("https://swapi.dev/api/films/3/");
+      setMovieInfo(info);
+    };
+    startWars();
+  }, []);
   return (
     <div>
-    	<h1>App</h1>
+      <h1>App</h1>
+      <h2>{movieInfo && movieInfo.data.director}</h2>
+      <h2>{movieInfo && movieInfo.data.title}</h2>
     </div>
   );
-}
-
+};
 export default App;
