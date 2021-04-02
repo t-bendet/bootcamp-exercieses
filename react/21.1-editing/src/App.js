6811cf0991a9beda649fd3dbe252b1b088f0e0d2
t-bendet <talbendet21@gmail.com>
2021-04-02 16:05:34 +0300

    21.1

--- react/21.1-editing/src/App.js
+++ react/21.1-editing/src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from "react";
 
-const App = ()=> {
+const App = () => {
+  const [toggle, setToggle] = useState(true);
+  const ref = useRef();
+  useEffect(() => {
+    if (ref.current) {
+      ref.current.focus();
+    }
+  }, [toggle]);
   return (
     <div>
-    	<h1>App</h1>
+      <h1>App</h1>
+      {toggle ? null : <input ref={ref}></input>}
+      <button onClick={() => setToggle(!toggle)}>
+        {toggle ? "edit" : "save"}
+      </button>
     </div>
   );
-}
+};
 
 export default App;
