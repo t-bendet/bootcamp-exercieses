fc8471dd092311c053de9617efd48702596407f4
t-bendet <talbendet21@gmail.com>
2021-04-02 16:49:56 +0300

    21.2

--- react/21.2-play pause video/src/App.js
+++ react/21.2-play pause video/src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
-
-const App = ()=> {
+import React, { useRef } from "react";
+import waves from "./waves.mp4";
+const App = () => {
+  const ref = useRef();
   return (
     <div>
-    	<h1>App</h1>
+      <h1>App</h1>
+      <video ref={ref} width="420" height="240">
+        <source src={waves} type="video/mp4" />
+      </video>
+      <div>
+        <button onClick={() => ref.current.play()}>Play</button>
+        <button onClick={() => ref.current.pause()}>Pause</button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
