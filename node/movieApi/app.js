8a4c54d63f48522c6bbaf455d33cebcc45a8df34
t-bendet <talbendet21@gmail.com>
2021-04-14 15:26:29 +0300

    update...

--- node/movieApi/app.js
+++ node/movieApi/app.js
@@ -1,12 +1,42 @@
 const express = require("express");
-const uniqid = require("uniqid");
-const hbs = require("hbs");
-
+const { getMoviesList, addMovie, getMovie, deleteMovie } = require("./utils");
+const app = express();
 app.use(express.json());
-app.get("app/user/:id", (req, res) => {
-  console.log(req.params);
-  res.render("index", {
-    title: "Weather",
-    name: "Andrew Mead",
-  });
+// TODO add status codes and error handling
+
+//get all Movies
+app.get("/api/movies", (req, res) => {
+  res.send(getMoviesList());
+});
+
+//Add a movie
+app.post("/api/movies", (req, res) => {
+  addMovie(req.body);
+  res.send("Got a POST request");
+});
+
+//get a single movie
+app.get("/api/movies/:id", (req, res) => {
+  const { id } = req.params;
+  res.send(getMovie(id));
+});
+
+//Update an existing movie
+app.put("/api/movies/:id", (req, res) => {
+  const { id } = req.params;
+  deleteMovie(id);
+  addMovie(req.body);
+  res.send("Got a Put request");
+});
+
+//Delete an existing movie
+app.delete("/api/movies/:id", (req, res) => {
+  const { id } = req.params;
+  deleteMovie(id);
+  res.send("DELETED FOREVER!!!!!!!!");
+});
+
+// PORT
+app.listen(3000, () => {
+  console.log("Server is up on port 3000.");
 });
